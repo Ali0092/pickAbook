bb63dbdff63a2a7ad71594db7af57bc7864dc908
Ali <aligondal8750@gmail.com>
2022-07-07 01:38:47 +0500

    third Commit

---
+++ app/src/main/java/com/example/pickabook/models/BookItem.kt
@@ -0,0 +1,9 @@
+package com.example.pickabook.models
+
+data class BookItem(
+    val title:String= "",
+    val author:String= "",
+    val status:String="",
+    val link:String="",
+    val price:String=""
+)
