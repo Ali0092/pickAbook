10282b9e1480c09e065171fbc9b17f274aad66dd
Ali <aligondal8750@gmail.com>
2022-07-08 20:28:42 +0500

    fourth Commit

--- app/src/main/java/com/example/pickabook/screens/about/AboutApp.kt
+++ app/src/main/java/com/example/pickabook/screens/about/AboutApp.kt
@@ -1,39 +1,37 @@
 package com.example.pickabook.screens.about
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
+import com.example.pickabook.DataUtils
 import com.example.pickabook.databinding.FragmentAboutAppBinding
 import com.example.pickabook.viewModel.AboutViewModel
-import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
+import com.squareup.picasso.Picasso
 
 class AboutApp : Fragment() {
 
-    private lateinit var binding:FragmentAboutAppBinding
-    private val vm:AboutViewModel by viewModels()
+    private lateinit var binding: FragmentAboutAppBinding
+    private val vm: AboutViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentAboutAppBinding.inflate(layoutInflater)
+        binding = FragmentAboutAppBinding.inflate(layoutInflater)
+
         vm.getSingleData()
         vm.data.observe(viewLifecycleOwner, Observer {
-         //   context?.let { it1 -> Glide.with(it1).load(it[0].link).into(binding.Image) }
-
-            binding.bookTitle.text=it[0].title.toString()
-            binding.authorName.text=it[0].author.toString()
-            binding.status.text=it[0].status.toString()
-            binding.price.text=it[0].price.toString()
+          //  Glide.with(this).load(it[0].link).into(binding.Image)
+            Picasso.get().load(it[0].link).into(binding.Image)
+            binding.bookTitle.text = it[0].title.toString()
+            binding.authorName.text = it[0].author.toString()
+            binding.status.text = it[0].status.toString()
+            binding.price.text = it[0].price.toString()
         })
         return binding.root
     }
