8716ce111097366d3ba36855c998a5f0b314474c
Ali <aligondal8750@gmail.com>
2022-07-15 22:37:48 +0500

    Eight Commit

---
+++ app/src/main/java/com/example/pickabook/screens/listOfBooks/BookListDiffUtils.kt
@@ -0,0 +1,33 @@
+package com.example.pickabook.screens.listOfBooks
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.pickabook.models.BookCatTitle
+
+class BookListDiffUtils(
+    private val oldList:List<BookCatTitle>,
+    private val newList:List<BookCatTitle>,
+):DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+             return oldList[oldItemPosition].name==newList[newItemPosition].name
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return when{
+            oldList[oldItemPosition].name!=newList[newItemPosition].name -> {
+                false
+            }
+            oldList[oldItemPosition].link!=newList[newItemPosition].link -> {
+                false
+            }
+            else -> true
+        }
+    }
+}
