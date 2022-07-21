c24e8f468c51431228b88ca5cbf5382f758cadf8
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2022-07-21 05:22:16 -0700

    Tenth

--- app/src/main/java/com/example/pickabook/viewModel/BookListViewModelFactory.kt
+++ app/src/main/java/com/example/pickabook/viewModel/BookListViewModelFactory.kt
@@ -2,17 +2,18 @@ package com.example.pickabook.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.Factory
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.pickabook.models.Category
 import java.lang.IllegalArgumentException
 import java.util.*
 
 
-class BookListViewModelFactory(private val cat:Category):ViewModelProvider.Factory {
-
+class BookListViewModelFactory(private val cat:Category):ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(BookListViewModel::class.java)){
-            return BookListViewModel(cat) as T
+            return  BookListViewModel(cat) as T
         }
-        throw IllegalArgumentException("Not Valid ViewModel....")
+        throw IllegalArgumentException("Invalid Arguments....")
     }
 }
